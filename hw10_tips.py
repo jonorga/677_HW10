00abb4c3b8cbc3f2452409bc6a1d93aeb35ae5d3
jonorgan <jonorgan26@gmail.com>
2023-11-13 23:18:35 -0500

    Finished tips assignment

--- hw10_tips.py
+++ hw10_tips.py
@@ -79,5 +79,59 @@ print("The correlation between tips and size of group is " + str(round(df_corr['
 print("\n")
 # Question 6 ========================================================================================
 print("Question 6:")
+total_smokers = df['size'][df['smoker'] == "Yes"].sum()
+total_non_smokers = df['size'][df['smoker'] == "No"].sum()
+all_people = total_smokers + total_non_smokers
+smokers_perc = str(round((total_smokers / all_people) * 100, 2))
+print(smokers_perc + "% of customers are smokers.")
+
+
+print("\n")
+# Question 7 ========================================================================================
+print("Question 7:")
+
+file_len = len(df.index)
+i = 0
+period_end = 0
+coefs = []
+
+while i < file_len:
+	cur_day = df['day'].iloc[i]
+	prev_day = df['day'].iloc[i]
+	period_start = i
+	while cur_day == prev_day:
+		cur_day = df['day'].iloc[i]
+		i += 1
+	period_end = i - 1
+
+	x = np.arange(1, period_end - period_start + 2)
+	y = df['tip_perc'][(df.index >= period_start) & (df.index <= period_end )].to_numpy()
+	z = np.polyfit(x, y, 1)
+	coefs.append(z[0])
+
+np_coefs = np.array(coefs)
+print("After applying linear regression to each day, and taking the average of the slope of each day"
+	+ " it would appear that tips are increasing each day with an average slope coefficient of "
+	+ str(np_coefs.mean()))
+
+
+print("\n")
+# Question 8 ========================================================================================
+print("Question 8:")
+df_smokers = df[df['smoker'] == "Yes"]
+df_non_smokers = df[df['smoker'] == "No"]
+smo_corr = df_smokers.corr()
+non_corr = df_non_smokers.corr()
+
+
+
+print("Yes, there is a difference in correlation between smokers and non-smokers.")
+print("Smokers:")
+print("Correlation between tip and total bill : " + str(smo_corr['tip'].iloc[0]))
+print("Correlation between tip and size : " + str(smo_corr['tip'].iloc[2]))
+print("Non-smokers:")
+print("Correlation between tip and total bill : " + str(non_corr['tip'].iloc[0]))
+print("Correlation between tip and size : " + str(non_corr['tip'].iloc[2]))
+
 
 
