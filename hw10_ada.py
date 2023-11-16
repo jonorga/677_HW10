7cdbf2b3865dfdf8baf085e04dbf4a9487451d99
jonorgan <jonorgan26@gmail.com>
2023-11-16 15:21:40 -0500

    Finished assignment

--- hw10_ada.py
+++ hw10_ada.py
@@ -88,21 +88,75 @@ print("\n")
 # Question 4 ========================================================================================
 print("Question 4:")
 
+def Ada(df, n, base_est):
+	ada = AdaBoostClassifier(n_estimators=n, base_estimator=base_est, learning_rate=0.5)
+	X_train = df[['Avg_Return', 'Volatility']][df['Week'] <= 50].values
+	Y_train = df[['Color']][df['Week'] <= 50].values
+	X_test = df[['Avg_Return', 'Volatility']][(df['Week'] > 50) & (df['Week'] <= 100)].values
+	actual = df[['Color']][(df['Week'] > 50) & (df['Week'] <= 100)].values
+	actual = actual.reshape(1, -1)[0]
+
+	ada = ada.fit(X_train, Y_train.ravel())
+	predictions = ada.predict(X_test)
+	i = 0
+	balance = 100
+	file_len = len(actual)
+	while i < file_len:
+		today_stock = balance / df['Close'].iloc[i + 50]
+		tmr_stock = balance / df['Close'].iloc[i + 51]
+		difference = abs(today_stock - tmr_stock)
+		if actual[i] == predictions[i]:
+			balance += difference * df["Close"].iloc[i + 51]
+		else:
+			balance -= difference * df["Close"].iloc[i + 51]
+		i += 1
+	return round(balance, 2)
+
 dt_score = best_dt['Accuracy'].iloc[0]
 nb_score = best_nb['Accuracy'].iloc[0]
 lr_score = best_lr['Accuracy'].iloc[0]
 
 if dt_score > nb_score and dt_score > lr_score:
 	print("The decision tree classifier is the strongest base estimator for my data")
+	cmg_ada_balance = Ada(df_cmg, best_dt['n'].iloc[0], DT)
+	spy_ada_balance = Ada(df_spy, best_dt['n'].iloc[0], DT)
 elif nb_score > dt_score and nb_score > lr_score:
 	print("The naive bayesian classifier is the strongest base estimator for my data")
+	cmg_ada_balance = Ada(df_cmg, best_nb['n'].iloc[0], NB)
+	spy_ada_balance = Ada(df_spy, best_nb['n'].iloc[0], NB)
 else:
 	print("The logistic regression classifier is the strongest base estimator for my data")
+	cmg_ada_balance = Ada(df_cmg, best_lr['n'].iloc[0], LR)
+	spy_ada_balance = Ada(df_spy, best_lr['n'].iloc[0], LR)
 
 
 print("\n")
 # Question 5 ========================================================================================
 print("Question 5:")
+
+def BNH(df):
+	y2_start = df['Close'].iloc[50]
+	y2_end = df['Close'].iloc[100]
+	stock = 100 / y2_start
+	return round(stock * y2_end, 2)
+
+cmg_bnh_balance = BNH(df_cmg)
+spy_bnh_balance = BNH(df_spy)
+
+if cmg_ada_balance > cmg_bnh_balance:
+	cmg_result = "more"
+else:
+	cmg_result = "less"
+if spy_ada_balance > spy_bnh_balance:
+	spy_result = "more"
+else:
+	spy_result = "less"
+
+print("For year 2 of the Chipotle stock, Adaboost ($" + str(cmg_ada_balance) + ") was " 
+	+ cmg_result + " effective than buy-and-hold ($" + str(cmg_bnh_balance) + ")")
+
+print("For year 2 of the S&P-500 stock, Adaboost ($" + str(spy_ada_balance) + ") was " 
+	+ spy_result + " effective than buy-and-hold ($" + str(spy_bnh_balance) + ")")
 
 
 
